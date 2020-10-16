c0f32afdb2f721ad91000e31b4aa040f839ea511
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-10-16 11:57:16 -0700

    API mods

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan  = require('morgan');
+const colors = require('colors');
+const connectDb = require('./config/db');
 
 // Load Env Vars
 dotenv.config({ path: './config/config.env' });
 
+
+// Connect to DB
+connectDb();
+
+// Route modules
+const bootcamp = require('./routes/bootcamp');
+
 const app = express();
+
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamp);
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+
+
+// Handle un-handled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+
+});
